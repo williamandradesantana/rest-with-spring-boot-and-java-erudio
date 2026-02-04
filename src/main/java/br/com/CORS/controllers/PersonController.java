9531b8444b849e13d01a333dbabb42885a502662
William Santana <williamandrade1058@gmail.com>
2026-02-04 11:52:10 -0300

    1803 Como Habilitar CORS no Spring Boot

--- src/main/java/br/com/CORS/controllers/PersonController.java
+++ src/main/java/br/com/CORS/controllers/PersonController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//@CrossOrigin(origins = "http://localhost:8080/")
 @RestController
 @RequestMapping("/api/person/v1")
 @Tag(name = "People", description = "Endpoints for Managing People")
@@ -19,7 +20,7 @@ public class PersonController implements PersonControllerDocs {
     @Autowired
     private PersonServices service;
 
-
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping(value = "/{id}", produces = {
             MediaType.APPLICATION_JSON_VALUE,
             MediaType.APPLICATION_XML_VALUE,
@@ -40,6 +41,7 @@ public class PersonController implements PersonControllerDocs {
         return service.findByAll();
     }
 
+    @CrossOrigin(origins = {"http://localhost:8080", "https://williamsantana-portfolio.vercel.app"})
     @PostMapping(
         produces = {
             MediaType.APPLICATION_JSON_VALUE,
