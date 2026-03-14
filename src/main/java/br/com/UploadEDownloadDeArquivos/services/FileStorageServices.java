b154a91233123733cad37d31dcdbc695fd021a5c
William Santana <williamandrade1058@gmail.com>
2026-03-14 11:34:40 -0300

    2104 Criando o Serviço Responsável por Armazenar o Arquivo em Disco

---
+++ src/main/java/br/com/UploadEDownloadDeArquivos/services/FileStorageServices.java
@@ -0,0 +1,47 @@
+package br.com.UploadEDownloadDeArquivos.services;
+
+import br.com.UploadEDownloadDeArquivos.config.FileStorageConfig;
+import br.com.UploadEDownloadDeArquivos.exception.FileStorageException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Service
+public class FileStorageServices {
+
+    private final Path fileStorageLocation;
+
+    @Autowired
+    public FileStorageServices(FileStorageConfig fileStorageConfig) {
+        Path path = Paths.get(fileStorageConfig.getUploadDir()).toAbsolutePath().normalize();
+
+        this.fileStorageLocation = path;
+
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception e) {
+            throw new FileStorageException("Could not create the directory where files will be stored", e);
+        }
+    }
+
+    public String storeFile(MultipartFile file) {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        try {
+            if (fileName.contains("..")) {
+                throw new FileStorageException("Sorry! Filename contains a invalid path sequence " + fileName);
+            }
+
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+            return fileName;
+        } catch (Exception e) {
+            throw new FileStorageException("Could not store file " + fileName + ". Please try again!", e);
+        }
+    }
+}
