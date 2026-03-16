aee0daeadbd2e4d66066b0163f3862f6fc0c641d
William Santana <williamandrade1058@gmail.com>
2026-03-16 09:29:40 -0300

    2109 Implementando o Download de Arquivo

--- src/main/java/br/com/UploadEDownloadDeArquivos/controllers/FileController.java
+++ src/main/java/br/com/UploadEDownloadDeArquivos/controllers/FileController.java
@@ -8,11 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -48,8 +47,24 @@ public class FileController implements FileControllerDocs {
         return Arrays.asList(files).stream().map(file -> uploadFile(file)).collect(Collectors.toList());
     }
 
+    @GetMapping("/downloadFile/{fileName:.+}")
     @Override
-    public ResponseEntity<Resource> downloadFile(String fileName, HttpServletRequest request) {
-        return null;
+    public ResponseEntity<Resource> downloadFile(@PathVariable("fileName") String fileName, HttpServletRequest request) {
+        Resource resource = services.loadFileAsResource(fileName);
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (Exception e) {
+            logger.error("Could not determine file type!");
+            throw new RuntimeException(e);
+        }
+
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 }
