45cf4447d7b74cdc38814924a03a17530cfac596
William Santana <williamandrade1058@gmail.com>
2026-03-19 10:17:04 -0300

    2214 Implementando o Exportador de CSV

--- src/main/java/br/com/ImportAndExportSpreadsheet/file/exporter/contract/FileExporter.java
+++ src/main/java/br/com/ImportAndExportSpreadsheet/file/exporter/contract/FileExporter.java
@@ -1,10 +1,10 @@
 package br.com.ImportAndExportSpreadsheet.file.exporter.contract;
 
-import br.com.ImportAndExportSpreadsheet.model.Person;
+import br.com.ImportAndExportSpreadsheet.data.dto.PersonDTO;
 import org.springframework.core.io.Resource;
 
 import java.util.List;
 
 public interface FileExporter {
-    Resource exportFile(List<Person> people) throws Exception;
+    Resource exportFile(List<PersonDTO> people) throws Exception;
 }
--- src/main/java/br/com/ImportAndExportSpreadsheet/file/exporter/impl/CsvExporter.java
+++ src/main/java/br/com/ImportAndExportSpreadsheet/file/exporter/impl/CsvExporter.java
@@ -1,16 +1,42 @@
 package br.com.ImportAndExportSpreadsheet.file.exporter.impl;
 
+import br.com.ImportAndExportSpreadsheet.data.dto.PersonDTO;
 import br.com.ImportAndExportSpreadsheet.file.exporter.contract.FileExporter;
-import br.com.ImportAndExportSpreadsheet.model.Person;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Component
 public class CsvExporter implements FileExporter {
     @Override
-    public Resource exportFile(List<Person> people) throws Exception {
-        return null;
+    public Resource exportFile(List<PersonDTO> people) throws Exception {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        OutputStreamWriter writer = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8);
+
+        CSVFormat csvFormat = CSVFormat.Builder.create()
+                .setHeader("ID", "First Name", "Last Name", "Address", "Gender", "Enabled")
+                .setSkipHeaderRecord(false)
+                .build();
+
+        try (CSVPrinter csvPrinter = new CSVPrinter(writer, csvFormat)) {
+            for (PersonDTO personDTO : people) {
+                csvPrinter.printRecord(
+                    personDTO.getId(),
+                    personDTO.getFirstName(),
+                    personDTO.getLastName(),
+                    personDTO.getAddress(),
+                    personDTO.getGender(),
+                    personDTO.getEnabled()
+                );
+            }
+        }
+        return new ByteArrayResource(outputStream.toByteArray());
     }
 }
