701e8fa199ae8cd1351d5e0d799459c2922dc9c5
William Santana <williamandrade1058@gmail.com>
2026-04-01 09:27:08 -0300

    2404 Lendo as Configurações de e-Mail Setadas no YML

---
+++ src/main/java/br/com/WorkingWithEmails/config/EmailConfig.java
@@ -0,0 +1,87 @@
+package br.com.WorkingWithEmails.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Objects;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.mail")
+public class EmailConfig {
+
+    private String host;
+    private int port;
+    private String username;
+    private String password;
+    private String from;
+    private boolean ssl;
+
+    public EmailConfig(){}
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public boolean isSsl() {
+        return ssl;
+    }
+
+    public void setSsl(boolean ssl) {
+        this.ssl = ssl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        EmailConfig that = (EmailConfig) o;
+        return port == that.port && ssl == that.ssl && Objects.equals(host, that.host) && Objects.equals(username, that.username) && Objects.equals(password, that.password) && Objects.equals(from, that.from);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(host);
+        result = 31 * result + port;
+        result = 31 * result + Objects.hashCode(username);
+        result = 31 * result + Objects.hashCode(password);
+        result = 31 * result + Objects.hashCode(from);
+        result = 31 * result + Boolean.hashCode(ssl);
+        return result;
+    }
+}
