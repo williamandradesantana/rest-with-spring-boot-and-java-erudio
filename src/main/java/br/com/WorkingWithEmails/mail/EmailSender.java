71c12dcb3825f0869b250080ae6e1296a8cf1e49
William Santana <williamandrade1058@gmail.com>
2026-04-01 10:13:38 -0300

    2407 Finalizando o e-Mail Sender

--- src/main/java/br/com/WorkingWithEmails/mail/EmailSender.java
+++ src/main/java/br/com/WorkingWithEmails/mail/EmailSender.java
@@ -1,8 +1,14 @@
 package br.com.WorkingWithEmails.mail;
 
+import br.com.WorkingWithEmails.config.EmailConfig;
+import jakarta.mail.MessagingException;
 import jakarta.mail.internet.AddressException;
 import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
@@ -12,6 +18,8 @@ import java.util.StringTokenizer;
 
 @Component
 public class EmailSender implements Serializable {
+
+    Logger logger = LoggerFactory.getLogger(EmailSender.class);
 
     private final JavaMailSender mailSender;
     private String to;
@@ -30,20 +38,49 @@ public class EmailSender implements Serializable {
         return this;
     }
 
-    public void setSubject(String subject) {
+    public EmailSender withSubject(String subject) {
         this.subject = subject;
+        return this;
     }
 
-    public void setBody(String body) {
+    public EmailSender withMessage(String body) {
         this.body = body;
+        return this;
     }
 
-    public void setRecipients(ArrayList<InternetAddress> recipients) {
-        this.recipients = recipients;
+    public EmailSender attach(String fileDir) {
+        this.attachment = new File(fileDir);
+        return this;
     }
 
-    public void setAttachment(File attachment) {
-        this.attachment = attachment;
+    public void send(EmailConfig config) {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(config.getUsername());
+            helper.setTo(recipients.toArray(new InternetAddress[0]));
+            helper.setSubject(subject);
+            helper.setText(body, true);
+
+            if (attachment != null) {
+                helper.addAttachment(attachment.getName(), attachment);
+            }
+            mailSender.send(message);
+            logger.info("Email send to %s with the subject '%s''%n", to, subject);
+            reset();
+        } catch (MessagingException e) {
+            throw new RuntimeException("Error sending the email", e);
+        }
+
+    }
+
+    private void reset() {
+        this.to = null;
+        this.subject = null;
+        this.body = null;
+        this.recipients = null;
+        this.attachment = null;
     }
 
     // email@gmail.com;email@email.com;email2@email.com
