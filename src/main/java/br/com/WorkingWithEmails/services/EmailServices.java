e096ba990f7ca31bf6f0396826d648c4dfd95e72
William Santana <williamandrade1058@gmail.com>
2026-04-01 12:20:16 -0300

    2412 Ajustando o Serviçe para Enviar e-Mails com Anexos

--- src/main/java/br/com/WorkingWithEmails/services/EmailServices.java
+++ src/main/java/br/com/WorkingWithEmails/services/EmailServices.java
@@ -3,8 +3,14 @@ package br.com.WorkingWithEmails.services;
 import br.com.WorkingWithEmails.config.EmailConfig;
 import br.com.WorkingWithEmails.data.dto.request.EmailRequestDTO;
 import br.com.WorkingWithEmails.mail.EmailSender;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @Service
 public class EmailServices {
@@ -20,5 +26,27 @@ public class EmailServices {
                 .withSubject(emailRequest.getSubject())
                 .withMessage(emailRequest.getBody())
                 .send(emailConfigs);
+    }
+
+    public void sendEmailWithAttachment(String emailRequestJson, MultipartFile attachment) {
+        File tempFile = null;
+        try {
+            EmailRequestDTO emailRequestDTO = new ObjectMapper().readValue(emailRequestJson, EmailRequestDTO.class);
+            tempFile = File.createTempFile("attachment", attachment.getOriginalFilename());
+            attachment.transferTo(tempFile);
+
+            emailSender
+                    .to(emailRequestDTO.getTo())
+                    .withSubject(emailRequestDTO.getSubject())
+                    .withMessage(emailRequestDTO.getBody())
+                    .attach(tempFile.getAbsolutePath())
+                    .send(emailConfigs);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error parse email request json!", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Error processing the attachment!", e);
+        } finally {
+            if (tempFile != null && tempFile.exists()) tempFile.delete();
+        }
     }
 }
