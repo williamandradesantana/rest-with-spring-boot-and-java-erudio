02bc47da2c7e74da5eff751cc1c68bcf53cdccbc
William Santana <williamandrade1058@gmail.com>
2026-04-03 17:37:17 -0300

    2506 Implementando o UserRepository e o UserService

---
+++ src/main/java/br/com/AuthenticationJWTAndSpringSecurity/services/UserServices.java
@@ -0,0 +1,27 @@
+package br.com.AuthenticationJWTAndSpringSecurity.services;
+
+import br.com.AuthenticationJWTAndSpringSecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServices implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public UserServices(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByUsername(username);
+        if (user != null) return user;
+
+        throw new UsernameNotFoundException("Username: " + username + " not found!");
+    }
+}
