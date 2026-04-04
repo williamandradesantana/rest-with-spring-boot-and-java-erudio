b2a757a8704bf1716bca748c31274fdaad236634
William Santana <williamandrade1058@gmail.com>
2026-04-04 14:49:24 -0300

    2510 Implementando o JwtTokenFilter

--- src/main/java/br/com/AuthenticationJWTAndSpringSecurity/security/jwt/JwtTokenProvider.java
+++ src/main/java/br/com/AuthenticationJWTAndSpringSecurity/security/jwt/JwtTokenProvider.java
@@ -51,15 +51,14 @@ public class JwtTokenProvider {
     }
 
     private String getRefreshToken(String username, List<String> roles, Date now) {
-        Date refreshTokenValidity = new Date(now.getTime() + validityInMilliseconds);
+        Date refreshTokenValidity = new Date(now.getTime() + (validityInMilliseconds * 3));
 
         return JWT.create()
                 .withClaim("roles", roles)
                 .withIssuedAt(now)
                 .withExpiresAt(refreshTokenValidity)
                 .withSubject(username)
-                .sign(algorithm)
-                .toString();
+                .sign(algorithm);
     }
 
     private String getAccessToken(String username, List<String> roles, Date now, Date validity) {
@@ -71,8 +70,7 @@ public class JwtTokenProvider {
                 .withExpiresAt(validity)
                 .withSubject(username)
                 .withIssuer(issuerUrl)
-                .sign(algorithm)
-                .toString();
+                .sign(algorithm);
     }
 
     public Authentication getAuthentication(String token) {
