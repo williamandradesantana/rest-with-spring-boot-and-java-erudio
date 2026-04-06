77b53378593ee51b1b5dde46ad4a67a6a3506d34
William Santana <williamandrade1058@gmail.com>
2026-04-06 12:39:49 -0300

    2513 Implementando o Serviço Responsável Pela Autenticação

---
+++ src/main/java/br/com/AuthenticationJWTAndSpringSecurity/services/AuthServices.java
@@ -0,0 +1,35 @@
+package br.com.AuthenticationJWTAndSpringSecurity.services;
+
+import br.com.AuthenticationJWTAndSpringSecurity.data.dto.security.AccountCredentialsDTO;
+import br.com.AuthenticationJWTAndSpringSecurity.data.dto.security.TokenDTO;
+import br.com.AuthenticationJWTAndSpringSecurity.repository.UserRepository;
+import br.com.AuthenticationJWTAndSpringSecurity.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServices {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+    @Autowired
+    private UserRepository repository;
+
+    public ResponseEntity<TokenDTO> sign(AccountCredentialsDTO credentials) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(credentials.getUsername(), credentials.getPassword())
+        );
+        var user = repository.findByUsername(credentials.getUsername());
+        if (user == null)
+            throw new UsernameNotFoundException("Username " + credentials.getUsername() + " not found!");
+
+        var token = tokenProvider.createAccessToken(credentials.getUsername(), user.getRoles());
+        return ResponseEntity.ok(token);
+    }
+}
